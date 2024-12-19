5b870ffddcfabba5cf139d69d26adf8c8e6ab1a0
Nabeel Mansour <mikaeel1@hotmail.com>
2024-12-19 20:33:13 +0100

    initialized script.js

---
+++ script.js
@@ -0,0 +1,39 @@
+// G-Version - Split the Nota
+// Skriv din pseudokod innanför nedanstående kommentarsblock
+/*
+
+    function splitTheNota()
+        SET variable sum till INPUT från användaren                    
+        SET variable friends till INPUT från användaren               
+        SET variable tip till 0.10              
+        SET variable nota till uträkning av sum gånger 0.10 och dela med friends          
+        
+        Print Varje person ska betala variabel nota kr
+    END function
+
+*/
+
+// VG-Version - Lewis Carroll Word Puzzle
+// Skriv din pseudokod innanför nedanstående kommentarsblock
+// Startkoden är 100% frivillig och kan tas bort eller skrivas om
+/*
+
+play();
+
+function play()
+    SET variabel ordbok = [FOUR, FOUL, FOOL, FOOT, FORT, FORE, FIRE, FIVE]; // Innehåller ALLA ord i det engelska språket
+    SET variabel startOrd till "FOUR";
+    SET variabel slutordOrd till "FIVE";
+
+
+end function
+
+function isOneLetterApart(wordOne, wordTwo)
+    SET variabel diffCount till 0;
+
+    // Här behöver du skriva koden för din funktion
+
+    return diffCount === 1; // returnerar sant om endast en bokstav ändrats, annars falskt
+end function
+
+*/
