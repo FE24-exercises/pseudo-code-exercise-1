509c0fbd4c85a726d5d2282360ba6871b11ed42b
Nabeel Mansour <mikaeel1@hotmail.com>
2024-12-20 20:17:56 +0100

    script.js modified

--- script.js
+++ script.js
@@ -24,14 +24,23 @@ function play()
     SET variabel ordbok = [FOUR, FOUL, FOOL, FOOT, FORT, FORE, FIRE, FIVE]; // Innehåller ALLA ord i det engelska språket
     SET variabel startOrd till "FOUR";
     SET variabel slutordOrd till "FIVE";
-
-
+    print variabel startOrd
+    print variabel slutOrd
+    isOneLetterApart()
 end function
 
 function isOneLetterApart(wordOne, wordTwo)
     SET variabel diffCount till 0;
 
     // Här behöver du skriva koden för din funktion
+    IF wordOne och wordTwo har olika många bokstäver
+        PRINT Båda orden måste innehålla lika många bokstäver
+    END IF
+
+    FOR varje tecken i startOrd, räkna med index `i`
+        IF startOrd plats i är lika med slutOrd plats i
+            räkna upp variabel diffCount med 1
+    END FOR
 
     return diffCount === 1; // returnerar sant om endast en bokstav ändrats, annars falskt
 end function
